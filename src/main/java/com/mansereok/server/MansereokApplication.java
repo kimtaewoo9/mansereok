4f5679c5cb947e1bf005d32d0323c7ed68999858
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-09-15 09:41:00 +0900

    first commit

---
+++ src/main/java/com/mansereok/server/MansereokApplication.java
@@ -0,0 +1,13 @@
+package com.mansereok.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MansereokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MansereokApplication.class, args);
+	}
+
+}
